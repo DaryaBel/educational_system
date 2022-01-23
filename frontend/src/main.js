0ed94545aa4d2c8fa01c5b4430515f1c541e25d9
Darya Belyaeva <d.belyaeva1@gmail.com>
2022-01-23 19:03:24 +0300

    DEV: initialize new project

---
+++ frontend/src/main.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import store from "@/store";
+import router from "@/router";
+
+import { createProvider } from "@/apollo";
+
+import VueYandexMetrika from "vue-yandex-metrika";
+
+import App from "@/App.vue";
+import "./registerServiceWorker";
+
+Vue.config.productionTip = false;
+
+// more info: https://github.com/vchaptsev/vue-yandex-metrika
+// Vue.use(VueYandexMetrika, {
+//   id: process.env.VUE_APP_YANDEX_METRIKA,
+//   env: process.env.NODE_ENV,
+//   router
+// })
+
+new Vue({
+  router,
+  store,
+  provide: createProvider().provide(),
+  render: (h) => h(App),
+}).$mount("#app");
